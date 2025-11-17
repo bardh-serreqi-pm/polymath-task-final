a550fbe1cbbe187145c128ced28bcbb3af16bf37
bardh-serreqi-pm <bardh.serreqi@polymath.services>
2025-11-17 10:38:21 +0100

    domain update

--- ops/iac/modules/edge/main.tf
+++ ops/iac/modules/edge/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws.us_east_1]
+    }
+  }
+}
+
 locals {
   bucket_name = var.frontend_bucket_name
 
