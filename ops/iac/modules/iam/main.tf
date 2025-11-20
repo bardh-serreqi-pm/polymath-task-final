17285761766575fd67a36675bc89bdd10f181fd6
bardh-serreqi-pm <bardh.serreqi@polymath.services>
2025-11-20 16:46:25 +0100

    iam least privilege update

--- ops/iac/modules/iam/main.tf
+++ ops/iac/modules/iam/main.tf
@@ -2,8 +2,10 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
 
+data "aws_partition" "current" {}
+
 locals {
-  partition  = data.aws_caller_identity.current.partition
+  partition  = data.aws_partition.current.partition
   account_id = data.aws_caller_identity.current.account_id
   region     = data.aws_region.current.name
 
